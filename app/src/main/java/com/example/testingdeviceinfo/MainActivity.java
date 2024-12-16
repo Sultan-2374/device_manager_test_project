7eee39d796ab05e00582a3b8d04f587b7eb66392
Sultan Moulana <sultan.m@getapplova.com>
2024-12-16 09:43:50 +0530

    New: added a method trigger device info

--- app/src/main/java/com/example/testingdeviceinfo/MainActivity.java
+++ app/src/main/java/com/example/testingdeviceinfo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.testingdeviceinfo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import io.applova.health.beans.ClientInfo;
 import io.applova.health.job.DeviceInfoJob;
@@ -23,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
         // Create and initialize DeviceInfoJob
         DeviceInfoJob deviceInfoJob = new DeviceInfoJob();
         deviceInfoJob.initialize(clientInfo);  // Schedule job on app start
+
+        // Get the button from the layout
+        Button btnTriggerJob = findViewById(R.id.btnTriggerJob);
+
+        // Pass the button to the reusable method
+        setupJobTriggerButton(btnTriggerJob);
+    }
+
+    /**
+     * Sets up a button to trigger the DeviceInfoJob when clicked.
+     *
+     * @param button The button to set the click listener on.
+     */
+    private void setupJobTriggerButton(Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClientInfo clientInfo = new ClientInfo();
+                clientInfo.setContext(MainActivity.this);
+
+                DeviceInfoJob deviceInfoJob = new DeviceInfoJob();
+                deviceInfoJob.initialize(clientInfo);
+
+                Toast.makeText(MainActivity.this, "Job triggered!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
